85c4e2506bfb997bfb168c4cc8eb02e0eb77612d
Peter Rasmussen <peter@soundboks.com>
2023-02-13 22:38:37 +0100

    error

--- cmake/freertos.cmake
+++ cmake/freertos.cmake
@@ -16,8 +16,8 @@ set( FREERTOS_HEAP "4" CACHE STRING "" FORCE)
 
 set( FREERTOS_PORT "GCC_ARM_CM4F" CACHE STRING "" FORCE)
 
-set(CMAKE_C_FLAGS                 "-mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -mthumb --static -nostartfiles -fno-common" CACHE INTERNAL "")
-set(CMAKE_CXX_FLAGS               "-mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -mthumb --static -nostartfiles -fno-common" CACHE INTERNAL "")
+set(CMAKE_C_FLAGS                 "-mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -mthumb --static -nostartfiles -fno-common -Wundef" CACHE INTERNAL "")
+set(CMAKE_CXX_FLAGS               "-mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -mthumb --static -nostartfiles -fno-common -Wundef" CACHE INTERNAL "")
 
 
 FetchContent_MakeAvailable(freertos_kernel)
