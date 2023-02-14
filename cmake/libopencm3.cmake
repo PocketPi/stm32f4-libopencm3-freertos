4ce395483400c62f726735c40c52d7f1c4a7b1e1
Peter Rasmussen <peter@soundboks.com>
2023-02-14 13:34:09 +0100

    updates

--- cmake/libopencm3.cmake
+++ cmake/libopencm3.cmake
@@ -8,17 +8,17 @@ FetchContent_Declare( libopencm3
 FetchContent_MakeAvailable(libopencm3)
 FetchContent_GetProperties(libopencm3)
 
-add_custom_target(libopencm3 make -j TARGETS=stm32/f4 WORKING_DIRECTORY ${libopencm3_SOURCE_DIR})
+add_custom_target(libopencm3_make_target make -j WORKING_DIRECTORY ${libopencm3_SOURCE_DIR})
 
-add_library(stm32f4 STATIC IMPORTED)
-target_link_libraries(stm32f4 INTERFACE)
+add_library(libopencm3 STATIC IMPORTED)
+target_link_libraries(libopencm3 INTERFACE)
 
-set_property(TARGET stm32f4 PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${libopencm3_SOURCE_DIR}/include)
-set_property(TARGET stm32f4 PROPERTY IMPORTED_LOCATION ${libopencm3_SOURCE_DIR}/lib/libopencm3_stm32f4.a)
-add_dependencies(stm32f4 libopencm3)
-target_link_directories(stm32f4 INTERFACE ${libopencm3_SOURCE_DIR}/lib)
+set_property(TARGET libopencm3 PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${libopencm3_SOURCE_DIR}/include)
+set_property(TARGET libopencm3 PROPERTY IMPORTED_LOCATION ${libopencm3_SOURCE_DIR}/lib/libopencm3_stm32g0.a)
+add_dependencies(libopencm3 libopencm3_make_target)
+target_link_directories(libopencm3 INTERFACE ${libopencm3_SOURCE_DIR}/lib)
 
-target_compile_definitions(stm32f4 INTERFACE -DSTM32F4)
+target_compile_definitions(libopencm3 INTERFACE -DSTM32G0)
 
-set(CMAKE_C_FLAGS                 "-mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -mthumb --static -nostartfiles -fno-common" CACHE INTERNAL "")
-set(CMAKE_CXX_FLAGS               "-mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -mthumb --static -nostartfiles -fno-common" CACHE INTERNAL "")
+set(CMAKE_C_FLAGS                 "-mcpu=cortex-m0plus -mthumb -msoft-float --static -nostartfiles -fno-common" CACHE INTERNAL "")
+set(CMAKE_CXX_FLAGS               "-mcpu=cortex-m0plus -mthumb -msoft-float --static -nostartfiles -fno-common" CACHE INTERNAL "")
--- src/main.c
+++ src/main.c
@@ -1,26 +1,60 @@
-//#include <libopencm3/stm32/rcc.h>
-//#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/cm3/systick.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>
+
+/* monotonically increasing number of milliseconds from reset
+ * overflows every 49 days if you're wondering
+ */
+volatile uint32_t system_millis;
+
+/* Called when systick fires */
+void sys_tick_handler(void) {
+    system_millis++;
+}
+
+/* sleep for delay milliseconds */
+static void msleep(uint32_t delay) {
+    uint32_t wake = system_millis + delay;
+    while (wake > system_millis)
+        ;
+}
+
 /*
-static void clock_setup(void)
-{
-	rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_3V3_84MHZ]);
-
-	rcc_periph_clock_enable(RCC_GPIOA);
+ * systick_setup(void)
+ *
+ * This function sets up the 1khz "system tick" count. The SYSTICK counter is a
+ * standard feature of the Cortex-M series.
+ */
+static void systick_setup(void) {
+    /* clock rate / 1000 to get 1mS interrupt rate */
+    systick_set_reload(84000);
+    systick_set_clocksource(STK_CSR_CLKSOURCE_AHB);
+    systick_counter_enable();
+    /* this done last */
+    systick_interrupt_enable();
 }
 
-static void gpio_setup(void)
-{
-	gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO5);
+/* Set STM32 system clock to 168 MHz. */
+static void clock_setup(void) {
+    rcc_clock_setup(&rcc_clock_config[RCC_CLOCK_CONFIG_HSI_PLL_64MHZ]);
+
+    rcc_periph_clock_enable(RCC_GPIOC);
 }
-*/
-int main(void)
-{
-	//clock_setup();
-	//gpio_setup();
+
+static void gpio_setup(void) {
+    gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO6);
+}
+
+int main(void) {
+    clock_setup();
+    gpio_setup();
+    systick_setup();
 
     // The task scheduler is blocking, so we should never come here...
-    for (;;){
-        //gpio_toggle(GPIOA, GPIO5);	/* LED on/off */
+    for (;;) {
+        gpio_toggle(GPIOC, GPIO6); /* LED on/off */
+        msleep(1000);
     };
-	return 0;
+    return 0;
 }
